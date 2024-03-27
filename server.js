8908e57cf5865da0c163ad716b5f8bd33c69d53f
CalleAlexanderson <calle.alexanderson@telia.com>
2024-03-27 16:22:22 +0100

    Tar data från databas till index.ejs

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const port = process.env.port | 3000;
 const app = new express;
 app.set("view engine", "ejs"); //sätter view engine till ejs och använder views katalogen
 app.use(express.static("public")); //statiska filer tas från public katalogen
-app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //skapar en connection till databasen dt207glab1 med en user av samma namn
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: "localhost",
     user: "dt207glab1",
     password: "dt207g",
     database: "dt207glab1"
 });
 
-connection.connect((err) => {
+db.connect((err) => {
     if (err) {
         console.log("connect failed: " + err);
         return;
@@ -27,7 +27,15 @@ connection.connect((err) => {
 
 // lägger till en sökväg till index.ejs
 app.get("/", (req, res) => {
-    res.render("index")
+    db.query("SELECT * FROM course", (err, result) => {
+        if (err){
+            console.log(err.message);
+        } else {
+            res.render("index", {
+                courses: result
+            })
+        }
+      });
 })
 
 // lägger till en sökväg till about_site.ejs
